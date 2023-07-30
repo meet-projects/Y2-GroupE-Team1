cf4d35a8f458fb604a699ba15c3fac1054479262
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-07-30 13:25:41 +0300

    r

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyD3j7p217EVs74H4hI3fw_4Ny_z2AwGjXk",
+  "authDomain": "travelpack-88791.firebaseapp.com",
+  "databaseURL": "https://travelpack-88791-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "travelpack-88791",
+  "storageBucket": "travelpack-88791.appspot.com",
+  "messagingSenderId": "394939009464",
+  "appId": "1:394939009464:web:d331e26c4197a9f97c674d"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
