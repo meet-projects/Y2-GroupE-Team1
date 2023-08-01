761b093197687cf09f1c600e85e9d640cac7dfe8
sirajis24-meet <139681226+sirajis24-meet@users.noreply.github.com>
2023-08-01 20:01:05 +0300

    Email and HTML

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, render_template_string
 from flask import session as login_session
 from flask_mail import Mail, Message
 import pyrebase
-
+import random
+import string
 
 config = {
   "apiKey": "AIzaSyD3j7p217EVs74H4hI3fw_4Ny_z2AwGjXk",
@@ -33,38 +34,60 @@ mail = Mail(app)
 
 #Code goes below here
 
+@app.route('/' , methods = ['GET' , 'POST'])
+def home():
+    if request.method=='POST':
+        db.child('Email').push(request.form['email'])
+    letter = db.child('letter').get().val()
+    if letter == None:
+        return render_template('index.html')
+    return render_template('index.html' , letter = letter)
 
-@app.route('/' , methods = ['GET','POST'])
-def newsl():
-  if request.method=='POST':
-    db.child('Email').push(request.form['email'])
-  letter = db.child('letter').get().val()
-  if letter == None:
-    return render_template('newsletter.html')
-  return render_template('newsletter.html' , letter = letter)
+
+
+  
 
 @app.route('/admin' , methods=['GET' , 'POST'])
 def change():
   error = ""
   mails = db.child('Email').get().val()
   if request.method == 'POST':
-    news = request.form['newspaper']
-    # try: 
-    recipients=[]
-    db.child('letter').set({'newspaper': news})
+    news = request.form['text']
+    title = request.form['title']
+    recipients = []
+    unique_key = generate_random_key()
+    db.child('Letters').child(unique_key).set({'text': news, 'title' : title})
     for i in mails : 
       recipients.append(mails[i])
-    msg = Message('newsletter' , sender= ' kostande.siaga@gmail.com' , recipients = recipients)
-    msg.body = news
+    msg = Message('newsletter' , sender= 'kostande.siaga@gmail.com' , recipients = recipients)
+
+    message_body = f"""
+    <html>
+    <body>
+    <h1>{title}</h1>
+    <p>{news}</p>
+    <p><a href="travlerpack.org/news/{unique_key}">Read more on our website!</a></p>
+    </body>
+    </html>
+    """
+    msg.html = render_template_string(message_body)
     mail.send(msg)
-    return redirect(url_for('newsl'))
-    # except:
-    error = 'somthing went wrong'
-  if mails != None:
-    return render_template('letter.html' , mails = mails , error = error)
+    return redirect(url_for('home'))
   return render_template('letter.html' , error = error)
 
 
+def generate_random_key(length=8):
+    characters_pool = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters_pool) for _ in range(length))
+    return random_string
+
+
+@app.route('/news/<string:news_id>')
+def news(news_id):
+    if news_id not in db.child('Letters').get().val():
+        return "<h1>No article found</h1>"
+    article = db.child("Letters").child(news_id).get().val()
+    return render_template("news.html", article=article)
 
 
 #Code goes above here
